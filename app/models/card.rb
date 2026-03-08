8da401d757db90634e9b06bb67e5465f2a7417ff
Ryan Guelzo <r.guelzo88@gmail.com>
2026-03-07 16:15:49 -0800

    WIP: Setting the cards up as a class instead of calling from a DB.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,30 @@
-class Card < ApplicationRecord
+class Card
+    SUITS  = %w[S H D C].freeze
+    RANKS  = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
+    attr_reader :suit, :rank
+
+    def initialize(suit, rank)
+        @suit = suit
+        @rank = rank
+    end
+
+    #define the initial value of the card based on the rank.
+    def value
+        if %w[J Q K].include?(rank)
+            10
+        elsif rank == 'A'
+            11
+        else
+            rank.to_i
+        end
+    end
+
+    def to_h
+        { suit: suit, rank: rank }
+    end
+
+    def self.from_h(h)
+        new(h['suit'] || h[:suit], h['rank'] || h[:rank])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get "game/index"
-  get "card/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,8 +14,6 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   root "game#index"
-
-  resources :cards
 
   resources :game
 
