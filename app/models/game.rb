d58f74fb982486cd94dda34fd72e28e9f4862d26
Ryan Guelzo <r.guelzo88@gmail.com>
2026-03-08 11:04:27 -0700

    WIP: Handling betting step.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,13 @@
 class Game
-    #TODO Need to create a deck class to represent a deck of cards.
+    attr_accessor :deck, :player_hand, :dealer_hand, :step, :balance
 
     #start game by giving the player a starting balance.
     def def initialize(balance = 1000)
       @balance = balance
+      @deck = Deck.new
+      @step = :betting
+      @player_hand = Hand.new
+      @dealer_hand = Hand.new
     end
 
     # TODO Need to break game into steps
@@ -16,5 +20,12 @@ class Game
     # -Busted
     # -Done
 
-    
+    def bet(wager)
+        @balance -= wager
+    end
+
+    def hit
+        
+    end
+
 end
