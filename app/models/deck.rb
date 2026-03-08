49062666ed4c6a7c62b2d74ec0a9546b2ff5c30e
Ryan Guelzo <r.guelzo88@gmail.com>
2026-03-08 10:49:53 -0700

    Created the deck class. Made it single so there can only be one deck during the game.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -7,4 +7,17 @@ class Deck
         @cards.shuffle!
     end
 
+    def deal
+        @cards.pop
+    end
+
+    def to_a
+        @cards.map(&:to_h)
+    end
+
+    def self.from_a(array)
+        deck = allocate #using allocate instead of new so there is only ONE SINGLE instance of the deck.
+        deck.instance_variable_set( :@cards, array.map {|h| Card.from_h(h)})
+        deck
+    end
 end
