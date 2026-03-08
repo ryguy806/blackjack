750b245340c75c8f414bc838a4309ee7eba5b6c9
Ryan Guelzo <r.guelzo88@gmail.com>
2026-03-08 10:13:33 -0700

    Set up todo for the game class.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,20 @@
 class Game
+    #TODO Need to create a deck class to represent a deck of cards.
+
+    #start game by giving the player a starting balance.
+    def def initialize(balance = 1000)
+      @balance = balance
+    end
+
+    # TODO Need to break game into steps
+    # Steps
+    # -Bet
+    # -Deal
+    # -Split
+    # -Hit
+    # -Stand
+    # -Busted
+    # -Done
+
     
 end
